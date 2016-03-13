79a6662d9a389b7ba725953f96911c035abd5957
Taras Rodynenko <t.rodynenko@gmail.com>
2016-03-13 23:03:18 +0200

    change Timeline from iframe to JS library

--- js/beat.js
+++ js/beat.js
@@ -1,8 +1,5 @@
 // JS to Bit-Generation   --------------------------------------------------
-$("#first-page").height(window.innerHeight);
-$("#bit-def").height(window.innerHeight);
-$(".title-block").css("padding-top", window.innerHeight*0.2);
-$(".def-block").css("padding-top", window.innerHeight*0.15);
+
 //  Page move   -------------------------------------------------------------
 function pageMove(){
   window.scrollBy(0, 15);
@@ -13,8 +10,7 @@ function pageMove2(){
   if (window.pageYOffset < window.innerHeight*2) { requestAnimationFrame(pageMove2); }
 }
 
-$(".arrow-down").bind("click", pageMove);
-$(".arrow-down-forward").bind("click", pageMove2);
+
 //  Animation Control  ------------------------------------------------------
 function AnimationElementHandler(e){
     var el = $(e), tp = el.offset().top;
@@ -33,17 +29,23 @@ $(window).scroll(function(){
     };
   });
 });
+
 $(document).ready(function() {
-  $('#inneriframe').on('load', function(){
-    setTimeout(function(){
-      $('#inneriframe').contents().find('div.tl-text-content-container h2.tl-headline').css('font-size', '24px');
-      $('#inneriframe').contents().find('div.tl-text-content-container h2.tl-headline').css('line-height', '28px');
-      $('#inneriframe').contents().find('div.tl-text-content-container h2.tl-headline').css('font-family', '"Roboto", sans-serif');
-      $('#inneriframe').contents().find('div.tl-text-content p').css('font-family', '"Roboto", sans-serif');
-      $('#inneriframe').contents().find('div.tl-text-content p').css('font-size', '14px');
-      $('#inneriframe').contents().find('h3.tl-headline-date').css('font-size', '16px');
-      $('#inneriframe').contents().find('div.tl-credit').css('width', '25%');
-      $('#inneriframe').contents().find('div.tl-text').css('width', '75%');
-    }, 1500);
-  });
+  $("#first-page").height(window.innerHeight);
+  $("#bit-def").height(window.innerHeight);
+  $(".title-block").css("padding-top", window.innerHeight*0.2);
+  $(".def-block").css("padding-top", window.innerHeight*0.15);
+
+  $(".arrow-down").bind("click", pageMove);
+  $(".arrow-down-forward").bind("click", pageMove2);
+
+  var timeline = new TL.Timeline('timeline-embed', 'https://docs.google.com/spreadsheets/d/1kb4DsOz21qho7TEVwlIub5AJIoq9PmsPkYNBPvMNswI');
+
+  $('div.tl-text-content-container h2.tl-headline').css('font-size', '24px');
+  $('div.tl-text-content-container h2.tl-headline').css('line-height', '28px');
+  $('div.tl-slide-content').css('width', '100%');
+  $('div.tl-text-content p').css('font-size', '14px');
+  $('h3.tl-headline-date').css('font-size', '16px');
+  $('div.tl-credit').css('width', '25%');
+  $('div.tl-text').css('width', '75%');
 });
